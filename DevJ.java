2f618341d5724b4031ed2ed6d5c8cdd4780e8af2
Pratheush <pratheush@outlook.com>
2022-09-26 13:52:54 +0530

    Add files via upload

---
+++ DevJ.java
@@ -0,0 +1,15 @@
+package com.example.learninggitintellij;
+
+import java.util.Arrays;
+
+public class DevJ {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        // Pratheush > MonuDevJP ::: requesting add changes >> add SOP : springframework springboot
+        System.out.println("MonuDevJP spring application 5678");
+        
+        System.out.println("args = " + Arrays.deepToString(args));
+        System.out.println("Pratheush Spring Application - 1234");
+    }
+}
